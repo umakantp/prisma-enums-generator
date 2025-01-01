bf6c0f9a94c9f5434d55b41f7e66037a165cbb08
umakantmanu <umakant@manufactured.com>
2025-01-01 15:00:48 +0530

    Fix indentations

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,24 @@
 export interface PrismaEnumsGeneratorConfig {
-    /**
-     * The namespace to generate the types in.
-     *
-     * @default true
-     */
-    isExport: boolean | undefined,
+  /**
+   * The namespace to generate the types in.
+   *
+   * @default true
+   */
+  isExport: boolean | undefined,
 
-    /**
-     * Use commas to separate the enum values.
-     *
-     * @default true
-     */
-    useComma: boolean | undefined,
+  /**
+   * Use commas to separate the enum values.
+   *
+   * @default true
+   */
+  useComma: boolean | undefined,
 }
 
 export function parseConfig(
-config: Record<string, string | string[] | undefined>
+  config: Record<string, string | string[] | undefined>
 ): PrismaEnumsGeneratorConfig {
-return {
+  return {
     isExport: config.isExport ? String(config.isExport).toLowerCase().trim() === 'true' : true,
     useComma: config.useComma ? String(config.useComma).toLowerCase().trim() === 'true' : true,
-};
+  };
 }
